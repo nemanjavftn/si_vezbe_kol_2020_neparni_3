688ac934749b543c09bfb4412dc0718f43e98f34
Nenad Penezic <nenad.penezi264@gmail.com>
2020-12-03 21:24:21 +0100

    no message

---
+++ PresentationLayer/DataLayer/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Repository
+    {
+        public List<Car> InsertCar (Car newCar)
+        {
+            List<Car> carList = new List<Car>();
+            using(SqlConnection sqlConnection=new SqlConnection())
+            {
+
+            }
+            return carList;
+        }
+    }
+}
