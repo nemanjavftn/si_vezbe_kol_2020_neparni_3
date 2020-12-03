394e413c0ab10d12d62e071a14d1a249155bbd57
Danilo Korac <5-2018@ftn.edu.rs>
2020-12-03 21:28:00 +0100

    Kreirane odgovarajuce klase u DataAccessLayer-u kao i odgovarajuce metode unutar njih.

---
+++ PresentationLayer/DataAccessLayer/Models/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Car
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Model
+        {
+            get;
+            set;
+        }
+        public int ProductionYear
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return Id + ". " + Model + "- " + ProductionYear;
+        }
+
+    }
+}
