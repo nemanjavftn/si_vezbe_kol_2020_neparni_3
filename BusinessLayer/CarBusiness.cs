58db33b5d032edac5358f46aa8aa9aa9a39f13ee
Jelena Đinović <jelenadjinovic99@gmail.com>
2020-12-03 21:42:52 +0100

    Kreiran BusinessLayer sa odgovarajucim metodama.

---
+++ BusinessLayer/CarBusiness.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class CarBusiness
+    {
+        private readonly Cars cars;
+
+        public CarBusiness()
+        {
+            this.cars = new Cars();
+        }
+
+        public List<Car> GetAllCars()
+        {
+            return this.cars.GetAllCars();
+        }
+
+        public bool InsertCars(Car c)
+        {
+            if (this.cars.InsertCars(c) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
