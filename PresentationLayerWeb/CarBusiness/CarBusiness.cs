65838bd6d3087c1bed4dc21e84832ea836346418
Andjelija Zocevic <andjelija.zocevic@gmail.com>
2020-12-03 22:02:17 +0100

    Dodat BusinessLayer i neke klase

---
+++ PresentationLayerWeb/CarBusiness/CarBusiness.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.Models;
+
+namespace CarBusiness
+{
+    public class CarBusiness
+    {
+        private readonly CarRepository carRepository;
+
+        public CarBusiness()
+        {
+            this.carRepository = new CarRepository();
+        }
+
+        public List<Car> GetCars();
+        {
+            return this.carRepository.GetCars();
+        }
+
+        public bool InsertCar(Car c)
+        {
+            if (this.carRepository.InsertCar(c)>0)
+            {
+            return true;
+            }
+            return false;
+        }
+       
+    }
+}
--- PresentationLayerWeb/DataLayer/Models/Car.cs
+++ PresentationLayerWeb/DataLayer/Models/Car.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataLayer
+namespace DataLayer.Models
 {
-    public class Class1
+    class Car
     {
     }
 }
