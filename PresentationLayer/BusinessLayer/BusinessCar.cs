190df7498f6f2d7313073c8cd521550ca77d015c
Danilo Korac <5-2018@ftn.edu.rs>
2020-12-03 21:37:52 +0100

    Kreirana odgovarajuca klasa u BusinessLayer-u kao i njene metode.

---
+++ PresentationLayer/BusinessLayer/BusinessCar.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class BusinessCar
+    {
+        private readonly CarRepositorium carRepositorium;
+
+        public BusinessCar()
+        {
+            this.carRepositorium = new CarRepositorium();
+        }
+        public List<Car> GetAllVets()
+        {
+            return this.carRepositorium.GetAllCars();
+        }
+
+        public bool InsertVet(Car c)
+        {
+            if (this.carRepositorium.InsertCar(c) >0 )
+                return true;
+
+            return false;
+        }
+
+
+    }
+}
