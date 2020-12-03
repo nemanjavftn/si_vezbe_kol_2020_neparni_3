7bc28498053741abf33075b878a0f8ff5abb1598
Lazar Jevtic <mailzaskoolu@gmail.com>
2020-12-03 21:40:12 +0100

    Kreirana tabela cars

---
+++ dbo.Cars.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Cars] (
+    [Id]             INT           IDENTITY (1, 1) NOT NULL,
+    [Model]          NVARCHAR (50) NOT NULL,
+    [ProductionYear] INT           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
