86971f65b221ba4b6b95c9b99ae2b847a74b1d43
Nenad Penezic <nenad.penezi264@gmail.com>
2020-12-03 21:53:34 +0100

    no message

---
+++ PresentationLayer/BusinessLayer/BLayer.cs
@@ -0,0 +1,35 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class BLayer
+    {
+        public readonly Repository repository;
+
+        public BLayer()
+        {
+            this.repository = new Repository();
+        }
+
+        public List<Car> getAllCars()
+        {
+            return this.repository.GetAllCars();
+        }
+
+        public bool insertCar(Car car)
+        {
+            if (this.repository.InsertCar(car)>0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
--- PresentationLayer/DataLayer/Car.cs
+++ PresentationLayer/DataLayer/Car.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataLayer
 {
-    class Car
+    public class Car
     {
         public int Id { get; set; }
         public string Model { get; set; }
