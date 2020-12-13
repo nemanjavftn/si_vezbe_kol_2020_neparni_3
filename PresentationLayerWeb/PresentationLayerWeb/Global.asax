7f523c4dc466b51d71ca8219e0d8b6f7215161c9
Nemanja Vicovic <nemanja.vicovic@ftn.edu.rs>
2020-12-13 13:45:21 +0100

    Pocetna verzija

--- PresentationLayerWeb/PresentationLayerWeb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PresentationLayerWeb.Global" Language="C#" %>
--- dbo.Table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Cars]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Model] NVARCHAR(50) NOT NULL, 
-    [ProductionYear] INT NULL,
-
-)
